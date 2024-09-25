ac4fbe9d402f84fa9fddee3c61216bac57a27865
Julian <jjvll90@gmail.com>
2024-09-24 19:27:01 -0500

    SQLite basic example in VSCODE

---
+++ sql.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE USERS (
+--     id int(11) NOT NULL,
+--     username varchar(255) NOT NULL,
+--     password varchar(255) NOT NULL,
+--     PRIMARY KEY (id)
+-- )
+
+INSERT INTO USERS (id, username, password) 
+VALUES (3, 'waylon', 'dog-baby')
